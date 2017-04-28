a358f0af499dd5069dbdee2345ebfb67ba108d23
SkyaTura <antonio@emiolo.com>
2017-04-28 10:01:10 -0300

    Added missing arguments

--- composer.ps1
+++ composer.ps1
@@ -1,5 +1,11 @@
-set $path (Resolve-Path .\).Path
+set path (Resolve-Path .\).Path
+set ssh "$home\.ssh"
+set composer "$home\.composer"
 
-echo "Current working directory: "$path
+echo "Current working directory: $path"
 
-docker run -v "$path\:/app" composer/composer:alpine $args
+docker run --rm `
+-v "$composer\:/composer/" `
+-v "$ssh\:/root/.ssh" `
+-v "$path\:/app" `
+composer/composer:alpine $args
